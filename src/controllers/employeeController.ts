c58f35136617cf3a493fc3bf8d5172e1ce026e71
Ashwinkumar Khude <ashwinkumar.k@A4G411-PC.afourtech.pune>
2021-10-20 16:39:30 +0530

    Updated controller

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { Employee } from '../entities/employee';
+import { json } from 'stream/consumers';
 // import Joi from 'joi';
 
 class EmployeeController {
@@ -20,7 +21,27 @@ class EmployeeController {
     public static addEmployee = (request: express.Request, response: express.Response) => {
         // const {error} = this.validateEmployee(request.body);
         // if(error) return response.status(400).send(error.details[0].message);
+        let employeeObject:any = {};
+        let missingFeilds = []
+        if(request.body.firstName!= null)
+            employeeObject.firstName = request.body.firstName;
+        else
+            missingFeilds.push("firstName");
+        if(request.body.lastName!= null)
+            employeeObject.lastName = request.body.lastName;
+        else
+            missingFeilds.push("lastName");
+        if(request.body.age!= null)
+            employeeObject.age =request.body.age;
+        else
+            missingFeilds.push("age");
+        if(request.body.position!= null)
+            employeeObject.position = request.body.position;
+        else
+            missingFeilds.push("position");
         
+        if(missingFeilds.length != 0)
+             return response.send("Following feilds are missing ->" + missingFeilds.toString());
         Employee.create(
             {
                 firstName:request.body.firstName,
@@ -34,7 +55,18 @@ class EmployeeController {
     }
 
     public static updateEmployee = (request: express.Request, response: express.Response) => {
-        Employee.update({id:request.body.id},{firstName:request.body.firstName});
+        if(request.body.id==null) return response.send("Id is missing");
+        let updatedObject:any = {};
+        if(request.body.firstName!= null)
+            updatedObject.firstName = request.body.firstName;
+        if(request.body.lastName!= null)
+            updatedObject.lastName = request.body.lastName;
+        if(request.body.age!= null)
+            updatedObject.age =request.body.age;
+        if(request.body.position!= null)
+            updatedObject.position = request.body.position;
+        if(updatedObject==null) return response.send("No data has been sent for updation");
+        Employee.update({id:request.body.id},updatedObject);
         Employee.findOne(
             {where:
             {id:parseInt(request.body.id)}}).then((data) =>{
@@ -44,6 +76,7 @@ class EmployeeController {
     
     
     public static delteEmployee = (request: express.Request, response: express.Response) => {
+        if(request.body.id==null) return response.send("Id is missing");
         Employee.delete({id:request.body.id})
         response.send("Employee entry deleted");
     }
