550c52dfb58b9619b6ce4dd29963f2de83aa7455
Ashwinkumar Khude <ashwinkumar.k@A4G411-PC.afourtech.pune>
2021-10-26 23:52:11 +0530

    Added support for cors header

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes/router';
 import { intializeDB } from './db';
+import cors from 'cors';
 intializeDB();
 const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded());
+app.use(cors())
 app.use('/',router);
 
 app.listen(3000,() =>{
