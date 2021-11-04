9ee6490f6594f807654cbb4bba898e0364ab6328
Ashwinkumar Khude <ashwinkumar.k@A4G411-PC.afourtech.pune>
2021-11-04 10:59:19 +0530

    Update 4

--- src/db.ts
+++ src/db.ts
@@ -3,11 +3,11 @@ import {createConnection} from "typeorm";
 import { Employee } from "./entities/employee";
 import { Project } from "./entities/project";
 import { Team } from "./entities/team";
-export async function intializeDB(hostParameter:string,usernameParameter:string,passwordParameter:string,databaseParameter:string, ): Promise<void> {
+export async function intializeDB(hostParameter:string,usernameParameter:string,passwordParameter:string,databaseParameter:string,portParameter:any ): Promise<void> {
   await createConnection({
     type: "postgres",
     host: hostParameter,
-    port: 5432,
+    port: portParameter,
     username: usernameParameter,
     password: passwordParameter,
     database: databaseParameter,
